964de5e3688d00b3d656b10d798ff408982aeee3
deepthivaranaso01 <varanasi.deepthi@gmail.com>
2023-05-30 02:26:54 -0400

    JavaScript and React.js Assignment 3

---
+++ src/labs/a3/index.js
@@ -0,0 +1,30 @@
+import JavaScript from "./java-script";
+import Classes from "./classes";
+import Styles from "./styles";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-List";
+import ConditionalOutput from "./conditional-output";
+
+function Assignment3() {
+    return (
+        <div>
+            <h1>Assignment 3</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+            <JavaScript/>
+            <TodoItem/>
+            <TodoList/>
+        </div>
+    );
+}
+export default Assignment3;
+
+
+
+
+
+
+
+
+
