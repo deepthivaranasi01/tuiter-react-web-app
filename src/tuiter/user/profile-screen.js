c854bef3d2e540972601b61909d7d48cac9698d5
deepthivaranaso01 <varanasi.deepthi@gmail.com>
2023-06-12 14:36:50 -0400

    final-1

--- src/tuiter/user/profile-screen.js
+++ src/tuiter/user/profile-screen.js
@@ -1,76 +1,194 @@
 import React, { useState, useEffect } from "react";
+
 import { useSelector, useDispatch } from "react-redux";
+
 import { useNavigate } from "react-router";
-import { profileThunk, logoutThunk, updateUserThunk }
-  from "../services/auth-thunks";
+
+import { profileThunk, logoutThunk, updateUserThunk } from "../services/auth-thunks";
+
+
 
 
 function ProfileScreen() {
- const { currentUser } = useSelector((state) => state.user);
- console.log("currentuser"+currentUser)
- const [profile, setProfile] = useState({firstName: "",
 
- lastName: "",});
- console.log("profile"+profile)
- console.log("profile"+setProfile)
- const dispatch = useDispatch();
- const navigate = useNavigate();
- const save = () => { dispatch(updateUserThunk(profile)); };
+  const { currentUser } = useSelector((state) => state.user);
 
- const handleLogout = async () => {
-  try {
-    await dispatch(logoutThunk());
-    navigate("/tuiter/login");
-  } catch (e) {
-    alert(e);
-  }
- };
+  const [profile, setProfile] = useState({
 
- useEffect(() => {
+    firstName: "",
+
+    lastName: "",
+
+  });
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+
+
+
+  const save = () => {
+
+    console.log(profile);
+
+    dispatch(updateUserThunk(profile));
+
+  };
+
+
+
+
+  useEffect(() => {
 
     const fetchProfile = async () => {
 
       const { payload } = await dispatch(profileThunk());
-      console.log("payload"+payload)
 
       setProfile(payload);
 
     };
 
+
+
+
     fetchProfile();
 
   }, [dispatch]);
- console.log("ssss"+profile)
- return (   <div>
-    <h1>Profile Screen</h1>
-    {profile && (<div>
-      <div>
-       <label>First Name</label>
-       <input type="text" value={profile.firstName}
-        onChange={(event) => {
-         const newProfile = {
-          ...profile, firstName: event.target.value,
-         };
-         setProfile(newProfile);
-        }}/>
-      </div>
-      <div>
-       <label>Last Name</label>
-       <input type="text" value={profile.lastName}
-        onChange={(event) => {
-         const newProfile = {
-          ...profile, lastName: event.target.value,
-         };
-         setProfile(newProfile);
-        }}/>
-      </div></div>
-    )}
+
+
+
+
+  console.log("profile1", profile);
+
+
+
+
+  return (
+
     <div>
-    <button 
-     onClick={handleLogout} className="btn btn-primary mt-2" style = {{marginRight:50}}>Logout</button> 
-    <button onClick={save} className="btn btn-primary mt-2" > Save  </button>
+
+      <h1>Profile Screen</h1>
+
+      {profile && (
+
+        <div>
+
+          <div>
+
+            <label>First Name</label>
+
+            <input
+
+              type="text"
+
+              value={profile.firstName}
+
+              onChange={(event) => {
+
+                const newProfile = {
+
+                  ...profile,
+
+                  firstName: event.target.value,
+
+                };
+
+                setProfile(newProfile);
+
+              }}
+
+              style={{
+
+                padding: "5px",
+
+                border: "1px solid #ccc",
+
+                borderRadius: "4px",
+
+              }}
+
+            />
+
+          </div>
+
+          <div>
+
+            <label>Last Name</label>
+
+            <input
+
+              type="text"
+
+              value={profile.lastName}
+
+              onChange={(event) => {
+
+                const newProfile = {
+
+                  ...profile,
+
+                  lastName: event.target.value,
+
+                };
+
+                setProfile(newProfile);
+
+                console.log(newProfile);
+
+              }}
+
+              style={{
+
+                padding: "5px",
+
+                border: "1px solid #ccc",
+
+                borderRadius: "4px",
+
+              }}
+
+            />
+
+          </div>
+
+        </div>
+
+      )}
+
+      <button
+
+        onClick={() => {
+
+          dispatch(logoutThunk());
+
+          navigate("/tuiter/login");
+
+        }} className="btn btn-primary mt-2" style= {{marginRight:50}}
+
+      >
+
+        Logout
+
+      </button>
+
+      <button
+
+        onClick={save} className="btn btn-primary mt-2"
+
+      >
+
+        Save
+
+      </button>
+
     </div>
-   </div>
- )
+
+  );
+
 }
+
+
+
+
 export default ProfileScreen;
